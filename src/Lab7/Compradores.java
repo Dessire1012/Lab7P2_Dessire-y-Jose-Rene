18804a439d8adfa5ee200afb38154e0c9a4938f9
Dessi <dessireochoa12@gmail.com>
2021-11-26 13:46:04 -0600

    correccion compradores

--- src/Lab7/Compradores.java
+++ src/Lab7/Compradores.java
@@ -1,19 +1,40 @@
 
 package Lab7;
 
+import java.util.ArrayList;
+
 
 public class Compradores extends Usuarios{
+    private ArrayList <Accesorios> listaComprados = new ArrayList();
+    private int cantidadDinero;
 
     public Compradores() {
     }
 
-    public Compradores(String usuario, String contraseña, int edad) {
+    public Compradores(int cantidadDinero, String usuario, String contraseña, int edad) {
         super(usuario, contraseña, edad);
+        this.cantidadDinero = cantidadDinero;
+    }
+
+    public ArrayList<Accesorios> getListaComprados() {
+        return listaComprados;
+    }
+
+    public void setListaComprados(ArrayList<Accesorios> listaComprados) {
+        this.listaComprados = listaComprados;
+    }
+
+    public int getCantidadDinero() {
+        return cantidadDinero;
+    }
+
+    public void setCantidadDinero(int cantidadDinero) {
+        this.cantidadDinero = cantidadDinero;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Compradores{" + "listaComprados=" + listaComprados + ", cantidadDinero=" + cantidadDinero + '}';
     }
  
 }
